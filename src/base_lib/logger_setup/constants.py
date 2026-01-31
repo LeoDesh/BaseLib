f468e78f8c24d4f8eab83adaecb11347270c05df
MG16 <dere_de@gmx.at>
2026-01-31 18:04:06 +0100

    Moved config file for logger into logger_setup folder

--- src/base_lib/logger_setup/constants.py
+++ src/base_lib/logger_setup/constants.py
@@ -1,7 +1,9 @@
 import logging.config
 from base_lib.logger_setup.misc import load_config
+from pathlib import Path
 
-logging_config = load_config(r"config.json")
+BASE_DIR = Path(__file__).resolve().parent
+logging_config = load_config(BASE_DIR / "config.json")
 logging.config.dictConfig(config=logging_config)
 app_logger = logging.getLogger("app")
 ordinary_logger = logging.getLogger("ord")
